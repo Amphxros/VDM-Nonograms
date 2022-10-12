c83ae7f00b186f5e7f91e86a71c33482a6e32228
Amph Rubio Bellón <ampxrubio@gmail.com>
2022-10-12 12:30:54 +0200

    PCInput created

---
+++ p1/PCEngine/src/main/java/es/ucm/vdm/pcengine/PCInput.java
@@ -0,0 +1,67 @@
+package es.ucm.vdm.pcengine;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+
+import es.ucm.vdm.engine.Input;
+
+public class PCInput extends Input implements  MouseListener, KeyListener, MouseMotionListener {
+
+    public PCInput(){
+        mTouchEvents_= new ArrayList<TouchEvent>();
+    }
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+}
