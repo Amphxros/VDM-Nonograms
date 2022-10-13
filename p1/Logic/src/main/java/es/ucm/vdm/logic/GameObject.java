f33ef061e1825f244bdbe1179eaab12e3af17205
A. Rubio <ampxrubio@gmail.com>
2022-10-13 18:37:26 +0200

    Input base (#21)
    * Input base and a little correction of hierarchy in graphics
    
    * PCInput created
    
    * feat AndroidInput created
    
    * correcciones
    
    * fix: gitattributes and style
    
    Co-authored-by: Aura Román <kyradiscord@gmail.com>

--- p1/Logic/src/main/java/es/ucm/vdm/logic/GameObject.java
+++ p1/Logic/src/main/java/es/ucm/vdm/logic/GameObject.java
@@ -1,18 +1,16 @@
 package es.ucm.vdm.logic;
 
-import es.ucm.vdm.logic.Vector2D;
-
-public abstract class GameObject{ //base of all the elements of the game
+public abstract class GameObject { //base of all the elements of the game
 
     protected Vector2D position_;
 
-    public GameObject(){
+    public GameObject() {
         position_ = new Vector2D(0, 0);
     }
 
-    public void setPos(Vector2D pos){
-        position_.x=pos.x;
-        position_.y= pos.y;
+    public void setPos(Vector2D pos) {
+        position_.x = pos.x;
+        position_.y = pos.y;
     }
 
 
---
+++ p1/engine/src/main/java/es/ucm/vdm/engine/EventType.java
@@ -0,0 +1,10 @@
+package es.ucm.vdm.engine;
+
+public enum EventType {
+    PRESSED_DOWN,
+    PRESSED_UP,
+    MOVED,
+    CLICKED,
+    KEY_DOWN,
+    KEY_UP
+}
