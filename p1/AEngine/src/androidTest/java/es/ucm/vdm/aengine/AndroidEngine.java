36123ae13868ef9a65e7751a414f8ec46ea2482f
Amphxros <ampxrubio@gmail.com>
2022-10-20 19:32:08 +0200

    primitive engine

--- p1/AEngine/src/androidTest/java/es/ucm/vdm/aengine/AndroidEngine.java
+++ p1/AEngine/src/androidTest/java/es/ucm/vdm/aengine/AndroidEngine.java
@@ -1,17 +1,12 @@
-package es.ucm.vdm.pcengine;
+package es.ucm.vdm.aengine;
 
 import es.ucm.vdm.engine.Engine;
 import es.ucm.vdm.engine.IAudio;
 import es.ucm.vdm.engine.IGraphics;
 import es.ucm.vdm.engine.IInput;
 
-public class PCEngine extends Engine {
-
-    public PCEngine(IGraphics graphics, IAudio audio, IInput input) {
+public class AndroidEngine extends Engine {
+    public AndroidEngine(IGraphics graphics, IAudio audio, IInput input) {
         super(graphics, audio, input);
     }
-    public void run(){
-
-    }
-
 }
--- p1/desktop/src/main/java/es/ucm/vdm/desktop/Desktop.java
+++ p1/desktop/src/main/java/es/ucm/vdm/desktop/Desktop.java
@@ -1,10 +1,15 @@
 package es.ucm.vdm.desktop;
 
 import es.ucm.vdm.pcengine.PCEngine;
+import es.ucm.vdm.pcengine.PCGraphics;
+import es.ucm.vdm.pcengine.PCInput;
 
 public class Desktop {
 
     public static void main(String[] args){
-        PCEngine eng= new PCEngine();
+        PCGraphics graphics= new PCGraphics();
+        PCInput input= new PCInput();
+        PCEngine eng= new PCEngine(graphics,null,input);
+
     }
 }
