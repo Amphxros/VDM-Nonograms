8d63c2f4e24238b62e1bfe69623f50ea11cce105
Amph Rubio Bellón <ampxrubio@gmail.com>
2022-11-30 13:53:41 +0100

    Iconos tematicas

--- _P1/Logic/src/main/java/vdm/p1/logic/scenes/ThemeScene.java
+++ _P1/Logic/src/main/java/vdm/p1/logic/scenes/ThemeScene.java
@@ -17,7 +17,7 @@ import vdm.p1.logic.objects.Image;
 import vdm.p1.logic.objects.Text;
 
 public class ThemeScene extends Scene{
-	public ThemeScene(IEngine engine) {
+	public ThemeScene(IEngine engine, String theme) {
 		super(engine);
 		IFont font = engine.getGraphics().newFont("font/pico.ttf", 20, true);
 		IImage image = engine.getGraphics().newImage("image/glassPanel.png");
@@ -38,73 +38,10 @@ public class ThemeScene extends Scene{
 				.addChild(goBackButton);
 
 		GameObject description = new Padding(0.3, 0, 0, 0)
-				.addChild(new Text("Selecciona la tematica", font)
+				.addChild(new Text(theme, font)
 						.setHorizontalAlignment(HorizontalAlignment.CENTRE)
 						.setVerticalAlignment(VerticalAlignment.TOP));
 
-		Grid row0 = new Grid(3, FlowDirection.HORIZONTAL);
-		row0.setElement(0, new Padding(0.2)
-				.addChild(new CreateLevelButton(getEngine(), font, 4, 4)
-						.addChild(new Image(image)
-								.setHorizontalAlignment(HorizontalAlignment.CENTRE)
-								.setVerticalAlignment(VerticalAlignment.MIDDLE))
-				)
-		);
 
-		row0.setElement(1, new Padding(0.2)
-				.addChild(new CreateLevelButton(getEngine(), font, 5, 5)
-						.addChild(new Image(image)
-								.setHorizontalAlignment(HorizontalAlignment.CENTRE)
-								.setVerticalAlignment(VerticalAlignment.MIDDLE))
-				)
-
-		);
-
-		row0.setElement(2, new Padding(0.2)
-				.addChild(new CreateLevelButton(getEngine(), font, 5, 10)
-						.addChild(new Image(image)
-								.setHorizontalAlignment(HorizontalAlignment.CENTRE)
-								.setVerticalAlignment(VerticalAlignment.MIDDLE))
-				)
-
-		);
-
-		Grid row1 = new Grid(3, FlowDirection.HORIZONTAL);
-		row1.setElement(0, new Padding(0.2)
-				.addChild(new CreateLevelButton(getEngine(), font, 8, 8)
-						.addChild(new Image(image)
-								.setHorizontalAlignment(HorizontalAlignment.CENTRE)
-								.setVerticalAlignment(VerticalAlignment.MIDDLE))
-				)
-		);
-
-		row1.setElement(1, new Padding(0.2)
-				.addChild(new CreateLevelButton(getEngine(), font, 10, 10)
-						.addChild(new Image(image)
-								.setHorizontalAlignment(HorizontalAlignment.CENTRE)
-								.setVerticalAlignment(VerticalAlignment.MIDDLE))
-
-				)
-		);
-
-		row1.setElement(2, new Padding(0.2)
-				.addChild(new CreateLevelButton(getEngine(), font, 10, 15)
-						.addChild(new Image(image)
-								.setHorizontalAlignment(HorizontalAlignment.CENTRE)
-								.setVerticalAlignment(VerticalAlignment.MIDDLE))
-				)
-		);
-
-		Grid rows = new Grid(2, FlowDirection.VERTICAL);
-		rows.setElement(0, row0);
-		rows.setElement(1, row1);
-
-		GameObject table = new Padding(0.4, 0, 0.1, 0).addChild(rows);
-
-		GameObject padding = new Padding(0.04, 0.1).addChild(header).addChild(description).addChild(table);
-		GameObject container = new Container(400, 600).addChild(padding);
-		GameObject body = new Body(engine).addChild(container);
-
-		addGameObject(body);
 	}
 }
