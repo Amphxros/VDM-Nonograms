b034e5e86155eebd99bb6007862f973fbfa08924
Amph Rubio Bellón <ampxrubio@gmail.com>
2022-12-01 19:13:31 +0100

    life manager

--- _P1/Logic/src/main/java/vdm/p1/logic/objects/LifeManager.java
+++ _P1/Logic/src/main/java/vdm/p1/logic/objects/LifeManager.java
@@ -11,35 +11,50 @@ public class LifeManager extends GameObject {
 
 	IEngine engine;
 	int life_number;
+	IImage heartFill;
+	IImage heartEmpty;
+
 	public LifeManager(IEngine engine){
 		this.engine=engine;
 		this.life_number=3;
-		GameObject heart_left = new Image(engine.getGraphics().newImage("image/heartfill.png"))
-				.setHorizontalAlignment(HorizontalAlignment.LEFT)
-				.setVerticalAlignment(VerticalAlignment.TOP);
 
+		heartFill= engine.getGraphics().newImage("image/heartfill.png");
+		heartEmpty= engine.getGraphics().newImage("image/heartempty.png");
 
-		GameObject heart_center = new Image(engine.getGraphics().newImage("image/heartfill.png"))
-				.setHorizontalAlignment(HorizontalAlignment.CENTRE)
-				.setVerticalAlignment(VerticalAlignment.TOP);
-
-
-		GameObject heart_right = new Image(engine.getGraphics().newImage("image/heartfill.png"))
-				.setHorizontalAlignment(HorizontalAlignment.RIGHT)
-				.setVerticalAlignment(VerticalAlignment.TOP);
-
-
-		GameObject padding= new Padding(0.04,0.1)
-				.addChild(heart_left)
-				.addChild(heart_center)
-				.addChild(heart_right);
+		Padding padding= new Padding(0.04, 0.01);
+		for(int i=0;i<life_number;i++){
+			GameObject g= new Image(heartFill)
+					//casts the int (i+1) cause its NONE, LEFT, CENTRE, RIGHT
+					.setHorizontalAlignment(HorizontalAlignment.values()[i+1])
+					.setVerticalAlignment(VerticalAlignment.MIDDLE);
+			padding.addChild(g);
+		}
 
 		addChild(padding);
+
 	}
 
 	public boolean removeHeart(){
+
 		life_number--;
-		GameObject pass
+		GameObject heart= new Image(heartEmpty);
+
+		GameObject pad= getChildren().get(0); //gets the padding
+		pad.getChildren().remove(life_number); //removes the last filled heart
+		pad.addChild(heart); //adds an empty one
 		return life_number > 0;
+	}
+
+	/**
+	 * adds a heart and increase the life number useful when we watch an ad
+	 */
+	public void addHeart(){
+		GameObject heart= new Image(heartFill);
+
+		GameObject pad= getChildren().get(0); //gets the padding
+		pad.getChildren().remove(life_number); //removes the last filled heart
+		pad.addChild(heart); //adds a filled one
+		life_number++;
+
 	}
 }
