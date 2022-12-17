d23e62ad16edb3b686927304f746a6dda271c1d6
Aura Román <kyradiscord@gmail.com>
2022-12-17 17:40:59 +0100

    fix(Android): call setResolution before Logic instantiation

--- _P1/app/src/main/java/vdm/p1/nonograms/MainActivity.java
+++ _P1/app/src/main/java/vdm/p1/nonograms/MainActivity.java
@@ -25,9 +25,10 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(renderView);
 
 		engine = new AndroidEngine(renderView, this);
+		engine.getGraphics().setResolution(400, 600);
+
 		Logic logic = new Logic(engine);
 		engine.setLogic(logic);
-		engine.getGraphics().setResolution(400, 600);
 
 		//if the user enters by a notification
 		Intent intent = getIntent();
