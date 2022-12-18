ad985fe47364d350e1bc386af2574883ab4f3ac9
Amph Rubio Bellón <ampxrubio@gmail.com>
2022-12-18 13:13:27 +0100

    Sensores creados

--- _P1/AndroidEngine/src/main/java/vdm/p1/androidengine/AndroidSensors.java
+++ _P1/AndroidEngine/src/main/java/vdm/p1/androidengine/AndroidSensors.java
@@ -1,8 +1,13 @@
 package vdm.p1.androidengine;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
 import vdm.p1.engine.Sensors;
 
-public class AndroidSensors extends Sensors {
+public class AndroidSensors extends Sensors implements SensorEventListener {
 
 	public AndroidSensors(){
 		super();
@@ -21,6 +26,43 @@ public class AndroidSensors extends Sensors {
 
 	@Override
 	public void unregister() {
+
+	}
+
+
+	/**public void onSensorChanged(int sensor, float[] values) {
+		System.out.println(sensor);
+		if (sensor == SensorManager.SENSOR_ACCELEROMETER) {
+			long curTime = System.currentTimeMillis();
+			// only allow one update every 100ms.
+			if ((curTime - lastUpdate) > 100) {
+				long diffTime = (curTime - lastUpdate);
+				lastUpdate = curTime;
+
+				setX(values[SensorManager.AXIS_X]);
+				setY(values[SensorManager.AXIS_Y]);
+				setZ(values[SensorManager.AXIS_Z]);
+
+				float speed = Math.abs(getX()+getY()+getZ() - last_x - last_y - last_z) / diffTime * 10000;
+
+				if (speed > SHAKE_THRESHOLD) {
+					Log.d("sensor", "shake detected w/ speed: " + speed);
+					Toast.makeText(this, "shake detected w/ speed: " + speed, Toast.LENGTH_SHORT).show();
+				}
+				last_x = x;
+				last_y = y;
+				last_z = z;
+	 		}
+		 */
+
+
+	@Override
+	public void onSensorChanged(SensorEvent sensorEvent) {
+
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int i) {
 
 	}
 }
